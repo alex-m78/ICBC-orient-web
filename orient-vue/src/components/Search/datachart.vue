a0614dfe4f5fe4178dbc87ffffff57edb75faf13
hsj <hsj_sukey@163.com>
2020-08-19 00:33:14 +0800

    原始数据页面修改

--- orient-vue/src/components/Search/datachart.vue
+++ orient-vue/src/components/Search/datachart.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
+    <div class="panel" id = "tab1">
     <el-table
         :data="stockData"
-        border
-        style="width: 100%">
-      <el-table-column fixed  prop="tsCode"  label="股票代码"></el-table-column>
-      <el-table-column  prop="name"  label="股票名称"> </el-table-column>
-      <el-table-column  prop="area"  label="所在地域"> </el-table-column>
-      <el-table-column  prop="industry"  label="所属行业"> </el-table-column>
-      <el-table-column  prop="market"  label="市场类型"> </el-table-column>
-      <el-table-column  prop="listDate"  label="上市日期"> </el-table-column>
+        border >
+      <el-table-column fixed  prop="tsCode"  label="股票代码" width="150"></el-table-column>
+      <el-table-column  prop="name"  label="股票名称" > </el-table-column>
+      <el-table-column  prop="area"  label="所在地域" > </el-table-column>
+      <el-table-column  prop="industry"  label="所属行业" > </el-table-column>
+      <el-table-column  prop="market"  label="市场类型" > </el-table-column>
+      <el-table-column  prop="listDate"  label="上市日期" > </el-table-column>
       <el-table-column  prop="setupDate"  label="公司成立时间"> </el-table-column>
       <el-table-column  prop="employees"  label="员工人数"> </el-table-column>
       <el-table-column  prop="qeps"  label="每股收益(单季度)"> </el-table-column>
@@ -28,7 +28,16 @@
       <el-table-column  prop="profitToGr"  label="净利润/营业总收入"> </el-table-column>
       <el-table-column  prop="qprofitToGr"  label="净利润/营业总收入(单季度)"> </el-table-column>
       <el-table-column  prop="opOfGr"  label="营业利润/营业总收入"> </el-table-column>
-      <el-table-column  prop="qopToGr"  label="营业利润/营业总收入(单季度)"> </el-table-column>
+      <el-table-column  fixed="right" prop="qopToGr"  label="营业利润/营业总收入(单季度)"> </el-table-column>
+
+    </el-table>
+    </div>
+    <el-divider><i class="el-icon-s-flag"></i></el-divider>
+    <div class="panel" id = "tab2">
+    <el-table
+        :data="stockData"
+        border >
+      <el-table-column fixed  prop="tsCode"  label="股票代码" width="150"></el-table-column>
       <el-table-column  prop="roe"  label="净资产收益率"> </el-table-column>
       <el-table-column  prop="qroe"  label="净资产收益率(单季度)"> </el-table-column>
       <el-table-column  prop="roeWaa"  label="邮加权平均净资产收益率编"> </el-table-column>
@@ -49,30 +58,41 @@
       <el-table-column  prop="qsalesYoy"  label="营业收入同比增长率(单季度)"> </el-table-column>
       <el-table-column  prop="qprofitYoy"  label="净利润同比增长率(单季度)"> </el-table-column>
       <el-table-column  prop="equityYoy"  label="净资产同比增长率"> </el-table-column>
-      <el-table-column  prop="label"  label="是否为基金重仓股"> </el-table-column>
-      <el-table-column  prop="high"  label="季最高价"> </el-table-column>
-      <el-table-column  prop="low"  label="季最低价"> </el-table-column>
-      <el-table-column  prop="vol"  label="季成交量"> </el-table-column>
-      <el-table-column  prop="pctChg0"  label="月涨跌幅(复权)_0"> </el-table-column>
-      <el-table-column  prop="pctChg1"  label="月涨跌幅(复权)_1"> </el-table-column>
-      <el-table-column  prop="pctChg2"  label="月涨跌幅(复权)_2"> </el-table-column>
-      <el-table-column  prop="turnoverRateMean"  label="换手率_mean"> </el-table-column>
-      <el-table-column  prop="peTtmAmin"  label="最小市盈率TTM"> </el-table-column>
-      <el-table-column  prop="peTtmAmax"  label="平均市盈率TTM"> </el-table-column>
-      <el-table-column  prop="peTtmMean"  label="最大市盈率TTM"> </el-table-column>
-      <el-table-column  prop="pbAmin"  label="最小市净率"> </el-table-column>
-      <el-table-column  prop="pbAmax"  label="平均市净率"> </el-table-column>
-      <el-table-column  prop="pbMean"  label="最大市净率"> </el-table-column>
-      <el-table-column  prop="psTtmAmin"  label="最小市销率（TTM）"> </el-table-column>
-      <el-table-column  prop="psTtmAmax"  label="平均市销率（TTM）"> </el-table-column>
-      <el-table-column  prop="psTtmMean"  label="最大市销率（TTM）"> </el-table-column>
-      <el-table-column  prop="dvTtmMean"  label="股息率TTM_mean"> </el-table-column>
-      <el-table-column  prop="totalShareMean"  label="总股本(万)"> </el-table-column>
-      <el-table-column  prop="totalMvMean"  label="总市值(万)"> </el-table-column>
-      <el-table-column  prop="floatShareMean"  label="流通股本(万)"> </el-table-column>
-      <el-table-column  prop="circMvMean"  label="流通市值(万)"> </el-table-column>
-      <el-table-column  prop="closeStd"  label="波动率"> </el-table-column>
+      <el-table-column  fixed="right" prop="closeStd"  label="波动率"> </el-table-column>
     </el-table>
+    </div>
+    <el-divider><i class="el-icon-s-flag"></i></el-divider>
+    <div class="panel" id = "tab3">
+      <el-table
+          :data="stockData"
+          :cell-class-name="cellClassName"
+          border >
+        <el-table-column fixed  prop="tsCode"  label="股票代码" width="150"></el-table-column>
+        <el-table-column  prop="high"  label="季最高价"> </el-table-column>
+        <el-table-column  prop="low"  label="季最低价"> </el-table-column>
+        <el-table-column  prop="vol"  label="季成交量"> </el-table-column>
+        <el-table-column  prop="pctChg0"  label="月涨跌幅(复权)_0"> </el-table-column>
+        <el-table-column  prop="pctChg1"  label="月涨跌幅(复权)_1"> </el-table-column>
+        <el-table-column  prop="pctChg2"  label="月涨跌幅(复权)_2"> </el-table-column>
+        <el-table-column  prop="turnoverRateMean"  label="换手率_mean"> </el-table-column>
+        <el-table-column  prop="peTtmAmin"  label="最小市盈率TTM"> </el-table-column>
+        <el-table-column  prop="peTtmAmax"  label="平均市盈率TTM"> </el-table-column>
+        <el-table-column  prop="peTtmMean"  label="最大市盈率TTM"> </el-table-column>
+        <el-table-column  prop="pbAmin"  label="最小市净率"> </el-table-column>
+        <el-table-column  prop="pbAmax"  label="平均市净率"> </el-table-column>
+        <el-table-column  prop="pbMean"  label="最大市净率"> </el-table-column>
+        <el-table-column  prop="psTtmAmin"  label="最小市销率（TTM）"> </el-table-column>
+        <el-table-column  prop="psTtmAmax"  label="平均市销率（TTM）"> </el-table-column>
+        <el-table-column  prop="psTtmMean"  label="最大市销率（TTM）"> </el-table-column>
+        <el-table-column  prop="dvTtmMean"  label="股息率TTM_mean"> </el-table-column>
+        <el-table-column  prop="totalShareMean"  label="总股本(万)"> </el-table-column>
+        <el-table-column  prop="totalMvMean"  label="总市值(万)"> </el-table-column>
+        <el-table-column  prop="floatShareMean"  label="流通股本(万)"> </el-table-column>
+        <el-table-column  prop="circMvMean"  label="流通市值(万)"> </el-table-column>
+        <el-table-column  fixed="right" prop="label"  label="是否为基金重仓股"> </el-table-column>
+      </el-table>
+    </div>
+
   </div>
 
 </template>
@@ -89,11 +109,49 @@ export default {
     },
   },
   methods: {
-
+    cellClassName({rowIndex,columnIndex}) {
+      if (rowIndex ===1 && columnIndex === 22) {
+        return 'warning-cell';
+      }
+      return "";
+    }
     }
 }
 </script>
 
 <style scoped>
+.el-table.warning-cell{
+  background: #6886c5;
+}
+@media(max-width:1280px){
+  .el-table{
+    width: 1100px;
+    height: 200px;
+  }
+  /*#tab1 {*/
+  /*  height: 300px;*/
+  /*}*/
+  /*#tab2 {*/
+  /*  height: 300px;*/
+  /*}*/
+}
+@media(min-width:1280px){
+  .el-table{
+    width: 1200px;
+    height: 250px;
+  }
+}
+@media(min-width:1600px){
+  .el-table{
+    width: 1400px;
+    height: 300px;
+  }
+}
+@media(min-width:1920px){
+  .el-table{
+    width: 1600px;
+    height: 350px;
+  }
+}
 
 </style>

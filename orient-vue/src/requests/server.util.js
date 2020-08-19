2757f88d9204aea63ac25782d0f68b8e78521fb4
hsj <hsj_sukey@163.com>
2020-08-19 15:35:08 +0800

    ip修改

--- orient-vue/src/requests/server.util.js
+++ orient-vue/src/requests/server.util.js
@@ -9,8 +9,8 @@ const $axios = (options) => {
         // 默认配置 axios 实例
         const instance = axios.create({
             // 默认请求 ip
-            // baseURL: 'http://47.103.137.116:80/api',
-            baseURL: 'http://localhost:8000/api',
+            baseURL: 'http://47.103.137.116:80/api',
+            // baseURL: 'http://localhost:8000/api',
             headers: {
                 'Content-Type': 'application/json;charset=UTF-8'
             },
